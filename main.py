0c11a0a863606747bf75e1bb917ade933d1db971
torstenbergersen <52758041+torstenbergersen@users.noreply.github.com>
2023-10-01 09:36:58 -0400

    Create data point variables to keep track of player

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
+    #keep track of player position
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
     # telling tcod which font to use
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
